d88813852912a05d4f6de8861ccf75c91c4a252e
Chami Lamelas <chami.lamelas@gmail.com>
2023-10-28 15:53:53 -0400

    Update initssh.sh

--- initssh.sh
+++ initssh.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/github_key  <<<y >/dev/null 2>&1
 printf "Host github.com\nIdentityFile ~/.ssh/github_key\n" >> ~/.ssh/config
 echo Copy and paste to github.com/settings/ssh/new
